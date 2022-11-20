4746fd9a23bbca6b1e3de233ae8dbb6caf020056
Gabriele Albini <57110246+gabri-al@users.noreply.github.com>
2022-11-20 21:11:11 +0100

    Update 03_Playlists_Centroids.py

--- 03_Playlists_Centroids.py
+++ 03_Playlists_Centroids.py
@@ -12,3 +12,41 @@ corpus_0_list = corpus_0.split()
 
 centroid_0 = np.average([word_vectors[w] for w in corpus_0_list if w in model.wv.key_to_index], axis=0)
 print(word_vectors.similar_by_vector(centroid_0))
+
+## Save centroid words
+centroid_words_1 = []
+for i in np.arange(0, len(word_vectors.similar_by_vector(centroid_1)), 1):
+    word_ = word_vectors.similar_by_vector(centroid_1)[i][0]
+    centroid_words_1.append(word_)
+
+centroid_words_0 = []
+for i in np.arange(0, len(word_vectors.similar_by_vector(centroid_0)), 1):
+    word_ = word_vectors.similar_by_vector(centroid_0)[i][0]
+    centroid_words_0.append(word_)
+
+## Plot PCA on most representative words
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
+from sklearn.decomposition import PCA
+
+reps_ = centroid_words_1.copy()
+for w in centroid_words_0:
+    reps_.append(w)
+labels = np.array([1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0])
+reps_vec = []
+
+word_vectors = model.wv
+for w in reps_:
+    reps_vec.append(word_vectors[w])
+
+# Calculate PCA
+pca = PCA(n_components = 10)
+p1 = pca.fit_transform(reps_vec)
+pl_1 = p1[:10]
+pl_0 = p1[10:]
+plt.scatter(pl_1[:,0],pl_1[:,1], c='darkblue', label='1-Metal')
+plt.scatter(pl_0[:,0],pl_0[:,1], c='crimson', label='0-Pop')
+plt.title("Most representative words : PCA")
+plt.legend(prop={'size': 6})
+plt.rcParams['figure.dpi'] = 100
+plt.show()
